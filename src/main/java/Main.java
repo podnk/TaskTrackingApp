89bda4f6b0a6607feb1d8b317365ae8d35b5f572
podnk <podnklada@gmail.com>
2019-01-02 21:50:44 +0300

    init 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+
 
 public class Main extends Application
 {
+    private static final Logger log = Logger.getLogger(Main.class);
+
     public static void main(String[] args)
     {
         Application.launch(args);
@@ -15,33 +20,42 @@ public class Main extends Application
     public void start(Stage stage)
     {
         // установка надписи
-        Text text = new Text("Text test" +
+        Text testText = new Text("Text test" +
                 "\n" +
-                "test text" +
+                "test testText" +
                 "\n" +
                 "тест текста" +
                 "\n" +
                 "1234567890" +
                 "\n" +
                 "TEXT TEST_!@#$%^&((&");
-        text.setLayoutY(80);    // установка положения надписи по оси Y
-        text.setLayoutX(100);   // установка положения надписи по оси X
 
-        Group group = new Group(text);
+        testText.setLayoutY(80);    // установка положения надписи по оси Y
+        testText.setLayoutX(100);   // установка положения надписи по оси X
+        testText.setFont(Font.font("Colibri", 16));
+
+        Group group = new Group(testText);
 
         Scene scene = new Scene(group);
-        stage.setScene(scene);
-        stage.setTitle("Task Tracker");
-        stage.setWidth(800);
-        stage.setHeight(600);
-        stage.show();
+
+        init(stage, scene);
     }
 
     @Override
     public void stop() throws Exception
     {
-        System.out.println("Application stops");
+        log.info("application stopped");
         super.stop();
     }
 
+    private void init(Stage stage, Scene scene)
+    {
+        stage.setScene(scene);
+        stage.setTitle("Task Tracker");
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
 }
