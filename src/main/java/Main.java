54fe411e330e01057102877c7a9259ec12b24639
Denis Lada <podnklada@gmail.com>
2019-01-02 15:36:50 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,15 @@ public class Main extends Application
     public void start(Stage stage)
     {
         // установка надписи
-        Text text = new Text("Hello from JavaFX!");
+        Text text = new Text("Text test" +
+                "\n" +
+                "test text" +
+                "\n" +
+                "тест текста" +
+                "\n" +
+                "1234567890" +
+                "\n" +
+                "TEXT TEST_!@#$%^&((&");
         text.setLayoutY(80);    // установка положения надписи по оси Y
         text.setLayoutX(100);   // установка положения надписи по оси X
 
@@ -23,9 +31,17 @@ public class Main extends Application
 
         Scene scene = new Scene(group);
         stage.setScene(scene);
-        stage.setTitle("First Application");
-        stage.setWidth(300);
-        stage.setHeight(250);
+        stage.setTitle("Task Tracker");
+        stage.setWidth(800);
+        stage.setHeight(600);
         stage.show();
     }
+
+    @Override
+    public void stop() throws Exception
+    {
+        System.out.println("Application stops");
+        super.stop();
+    }
+
 }
